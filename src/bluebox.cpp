8fc23eb80ff475e59d9d11a0e76a06425bcb6045
Uwe Arzt <mail@uwe-arzt.de>
2015-09-29 14:02:40 +0200

    Added Bluebox Example, Video Examples

--- src/bluebox.cpp
+++ src/bluebox.cpp
@@ -23,20 +23,61 @@ int main(int argc, char* argv[]) {
 
     // -----------------------------------------------------------------------------
     // test 1 -> directly manipulate the RGB image
+     int i, j;
+    uchar r, g, b;
+
+    for (int i = 0; i < image.rows; ++i) {
+        // point to first pixel in row
+        cv::Vec3b* pixel = image.ptr<cv::Vec3b>(i);
+
+        for (int j = 0; j < image.cols; ++j) {
+            r = pixel[j][2];
+            g = pixel[j][1];
+            b = pixel[j][0];
+            //std::cout << "r: " << (uint16_t)r << " g: " << (uint16_t)g << " b: " << (uint16_t)b << std::endl;
+            if(r < 30 && g < 60 && b > 90) {
+              pixel[j][2] = 0;
+              pixel[j][1] = 255;
+              pixel[j][0] = 0;
+           }
+        }
+    }
+
+    result = image;
 
     // -----------------------------------------------------------------------------
     // test 2 -> convert to HSV and manipulate the Hue
-    // convert to HSV
-    cv::Mat hsv_image;
+    /* cv::Mat hsv_image;
     cv::cvtColor(image, hsv_image, CV_BGR2HSV);
 
     // split into channels (in this case HSV)
     cv::Mat channel[3];
-    cv::split(hsv_image, channel);
+    cv::split(hsv_image, channel); 
 
+    // now we have a single channel to work with
+    int i,j;
+    uchar* p;
+    for( i = 0; i < channel[0].rows; ++i)
+    {
+        p = channel[0].ptr<uchar>(i);
+        for ( j = 0; j < channel[0].cols; ++j)
+        {
+            //std::cout << "h: " << (uint16_t)p[j] << std::endl;
+            if(p[j] > 100 && p[j] < 120) {
+                p[j] = 255;
+            }
+        }
+    }
+
+    // rebuild
+    cv::Mat tmp;
+    cv::merge(channel, 3, tmp);
+    cv::cvtColor(tmp, result, CV_HSV2BGR); */
 
     // -----------------------------------------------------------------------------
     // show
+    cv::imwrite("tmp.jpg", result);
+
     cv::namedWindow("Bluebox", cv::WINDOW_AUTOSIZE);
     cv::imshow("Bluebox", result);
 
